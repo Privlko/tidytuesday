341e13ecce31861b3dcf25ba4ca3632aa383d9f9
Privlko <privalki@tcd.ie>
2019-09-28 13:42:19 +0100

    update after saving

--- 2019/week 33/simpsons.R
+++ 2019/week 33/simpsons.R
@@ -25,7 +25,47 @@ simpsons <- simpsons %>%
 simpsons %>% 
   filter(self) %>% 
   count(guest_star, sort=T) %>% 
-  filter(n>1)
+  filter(n>1) %>% 
+  mutate(guest_star = fct_reorder(guest_star , n)) %>% 
+  ggplot(aes(guest_star, n))+
+  geom_col()+
+  coord_flip()+
+  labs(title = "Who has appeared on The Simpsons \nas themselves most often?")
 
 
+simpsons %>% 
+  filter(str_detect(role, ';')) %>% 
+  add_count(role) 
 
+
+simpsons %>% 
+  separate_rows(role, sep= ";\\s+") %>% 
+  add_count(role)
+
+simpsons %>% 
+  separate_rows(role, sep= ";\\s+") %>% 
+  count(role, sort = T)
+
+simpsons
+
+
+simpsons %>% 
+  separate_rows(role, sep= ";\\s+") %>% 
+  add_count(role) %>% 
+  mutate(self=str_detect(role, 'self|selves')) %>% 
+  filter(n>12, self==F) %>% 
+  count(season= parse_number(season), role) %>% 
+  mutate(role = fct_reorder(role, -nn, sum)) %>% 
+  ggplot(aes(season, nn))+
+  geom_col()+
+  facet_wrap(~role)+
+  labs(title = 'Most common guest characters \nin The Simpsons',
+       subtitle = 'Guest characters who do not \nplay themselves are often the ones listed below',
+       y='Apearances',
+       x='Season',
+       caption='Source: Wikipedia \nPlot: @privlko' )
+
+
+simpsons
+
+ggsave('C:/Users/Ivan/Desktop/R projects/#tidytuesday/tidytuesday/2019/week 33/simpsons.jpg')

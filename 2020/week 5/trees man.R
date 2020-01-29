a4859192c662d81795c4f9b033ab22124356efb5
Ivan Privalko <Ivan.Privalko@esri.ie>
2020-01-29 16:13:33 +0000

    git to kanye

--- 2020/week 5/trees man.R
+++ 2020/week 5/trees man.R
@@ -12,22 +12,6 @@ sf_trees
 sf_trees %>% 
   count(legal_status)
 
-sf_trees %>% 
-  group_by(date, legal_status) %>% 
-  count() %>% 
-  arrange(desc(n)) %>% 
-  filter(!is.na(date),
-         legal_status!="Landmark tree",
-         legal_status!="Private",
-         legal_status!="Property Tree") %>% 
-  ggplot(aes(x=date,
-             y=n, group=legal_status,
-             col=legal_status))+
-  geom_point()+
-  geom_line()+
-  facet_wrap(~legal_status, scales = 'free_y')
-
-
 
 ###what about summarising some of this into a yearly data.
 
@@ -35,7 +19,8 @@ sf_trees %>%
   mutate(year = lubridate::year(date)) %>% 
   group_by(year, legal_status) %>% 
   count() %>% 
-  filter(!is.na(date),
+  arrange(desc(n)) %>% 
+  filter(!is.na(year),
          legal_status!="Landmark tree",
          legal_status!="Private",
          legal_status!="Property Tree") %>% 
@@ -44,5 +29,14 @@ sf_trees %>%
              col=legal_status))+
   geom_point()+
   geom_line()+
-  facet_wrap(~legal_status)
+  facet_wrap(~legal_status, scales = 'free')+
+  theme(legend.position="bottom")+
+  labs(x='Year',
+       y="Number of trees planted",
+       title="Number of trees peaked between 2010 and 2020 for most projects",
+       subtitle="Permitted sites are still the largest producers of new trees",
+       caption= "Source: data.sfgov.org \nPlot: @privlko",
+       colour= "Legal status")
 
+
+ggsave("C:/Users/Ivan.Privalko/Desktop/R projects/tidytuesday/2020/week 5/treesman.jpg")

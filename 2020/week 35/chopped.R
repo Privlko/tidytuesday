74acd2623f341cc0543505b6dfef2d9746c3fd93
privlko <privalki@tcd.ie>
2020-08-26 22:20:14 +0100

    chopped

---
+++ 2020/week 35/chopped.R
@@ -0,0 +1,63 @@
+library(tidytuesdayR)
+library(ggplot2)
+library(tidyverse)
+library(here)
+library(Hmisc)
+library(patchwork)
+
+# load the data  ----------------------------------------------------------
+
+df <- tt_load(2020, week = 35)
+
+
+df
+chopped <- df$chopped
+
+chopped
+
+p1 <- chopped %>% 
+  filter(!is.na(episode_rating)) %>%
+  ggplot(aes(x=season,
+           y=episode_rating))+
+  geom_jitter(alpha=0.25,
+              aes(col=factor(season)))+
+  stat_summary(fun = mean, geom = "point") + 
+  stat_summary(fun.data = mean_se, 
+               geom = "errorbar",
+               fun.args = list(mult = 2))+
+  scale_y_continuous(limits = c(5, 10))+
+  theme(legend.position = "none") 
+
+
+
+p1+labs(title = "Chopped season 35 saw a jump in ratings",
+        subtitle = "Average ratings for the season contain standard errors(*1.96)",
+        x="Season",
+        y="Episode rating",
+        caption = "Data: github.com/tidytuesday \nPlot: @privlko")
+
+
+here()
+ggsave(here("2020", "week 35", "chopped.png"))
+
+
+
+
+
+sum1 <- chopped %>%
+  filter(!is.na(episode_rating)) %>% 
+  group_by(season) %>%
+  summarise(mean_rating = mean(episode_rating),
+            sd_rating = sd(episode_rating),
+            n = n(),
+            se_rating = sd_rating/sqrt(n)) 
+
+sum1 %>% 
+  ggplot(aes(x=factor(season), 
+             y= mean_rating))+
+  geom_point()+
+  geom_errorbar(aes(ymin=mean_rating-(se_rating*1.96),
+                    ymax=mean_rating+(se_rating*1.96)))+
+  scale_y_continuous(limits = c(0, 10))+
+  theme_bw()
+
